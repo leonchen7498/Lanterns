fbc533ae71f04455b96d098958764f3f06d19404
Leon Chen <leonchen@live.nl>
2021-02-16 22:58:59 +0100

    Added the vine/spike stage hazard
    -On touching a spike the player's lantern will change sprite to show it's damaged state
    -Lantern has 3 lives, when the player lose all his lives then it'll respawn to the set checkpoint (I didn't create checkpoints yet, but the ability to set it's position is there)
    -Player sprite blinks when it takes damage

---
+++ Assets/Resources/Scripts/Constants.cs
@@ -0,0 +1,10 @@
+public static class Constants
+{
+    public static class Tags
+    {
+        public const string Player = "Player";
+        public const string Shadow = "Shadow";
+        public const string Hazard = "Hazard";
+    }
+}
+
---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int lives = 3;
+    public float invulnerableTimer = 3.0f;
+    public float blinkInterval = 0.2f;
+    public SpriteRenderer spriteRenderer;
+    public Sprite noDamageSprite;
+    public Sprite lightDamagedSprite;
+    public Sprite heavyDamagedSprite;
+
+    private bool invulnerable;
+    private int currentLives;
+    private Vector2 checkpointPosition;
+    private List<Collision2D> dangerousCollisions;
+    private bool spriteVisible = true;
+    private bool canBlink = true;
+    private Color spriteColor;
+
+    void Start()
+    {
+        currentLives = lives;
+        checkpointPosition = transform.position;
+        dangerousCollisions = new List<Collision2D>();
+        spriteColor = spriteRenderer.color;
+    }
+
+    void FixedUpdate()
+    {
+        if (invulnerable)
+        {
+            if (canBlink)
+            {
+                StartCoroutine(Blink());
+            }
+            return;
+        }
+
+        if (dangerousCollisions.Count > 0)
+        {
+            LoseLife();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == Constants.Tags.Hazard)
+        {
+            dangerousCollisions.Add(collision);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == Constants.Tags.Hazard)
+        {
+            dangerousCollisions.Remove(collision);
+        }
+    }
+
+    private void LoseLife()
+    {
+        currentLives--;
+
+        switch (currentLives)
+        {
+            case 2:
+                TakeDamage(lightDamagedSprite);
+                break;
+            case 1:
+                TakeDamage(heavyDamagedSprite);
+                break;
+            case 0:
+                Respawn();
+                break;
+        }
+    }
+
+    private void TakeDamage(Sprite sprite)
+    {
+        //maybe do something else? like instantly flashing light
+        spriteRenderer.sprite = sprite;
+        StartCoroutine(StartInvulnerableTimer());
+    }
+
+    private void Respawn()
+    {
+        //maybe play an animation?
+        transform.position = checkpointPosition;
+        spriteRenderer.sprite = noDamageSprite;
+        currentLives = lives;
+    }
+
+    private IEnumerator StartInvulnerableTimer()
+    {
+        invulnerable = true;
+
+        yield return new WaitForSeconds(invulnerableTimer);
+        
+        invulnerable = false;
+        spriteRenderer.color = spriteColor;
+    }
+
+    private IEnumerator Blink()
+    {
+        canBlink = false;
+
+        if (spriteVisible)
+        {
+            spriteRenderer.color = new Color(spriteColor.r, spriteColor.g, spriteColor.b, 0f);
+        }
+        else
+        {
+            spriteRenderer.color = spriteColor;
+        }
+
+        spriteVisible = !spriteVisible;
+
+        yield return new WaitForSeconds(blinkInterval);
+        canBlink = true;
+    }
+}

3d292e2150b781693b42b24bddb1ee52ab525dd4
Rune Reeves <rune.reeves@hotmail.com>
2021-02-17 03:00:29 +0100

    Lantern behaviour second pass
    Implemented second pass of lantern behaviour. There's some jitter, but that should be fixable.

--- Assets/Resources/Scripts/LanternBehaviour.cs
+++ Assets/Resources/Scripts/LanternBehaviour.cs
@@ -7,17 +7,12 @@ public class LanternBehaviour : MonoBehaviour
     public Rigidbody2D rb;
 
     private bool isActive = false;
-    private readonly float movementSpeed = 11;
+    public float movementSpeed = 11;
 
     private Transform target;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Start is called before the first frame update
-    void Start() {
-
     }
 
     // Update is called once per frame
@@ -30,16 +25,15 @@ public class LanternBehaviour : MonoBehaviour
     }
 
     private void ActiveBehaviour () {
-        Vector3 direction = target.position - transform.position;
-        rb.AddRelativeForce(direction.normalized * movementSpeed);
+
     }
 
     private void PassiveBehaviour () {
         //idk bob up and down or something
     }
 
-    private void Move(Vector2 velocity) {
-        transform.position += (Vector3)velocity * Time.deltaTime;
+    public void Move(Vector2 velocity) {
+        transform.position += (Vector3)velocity * movementSpeed * Time.deltaTime;
     }
 
     public void Activate (Transform target) {
--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -15,9 +15,13 @@ public class PlayerController : MonoBehaviour
     public float dashCooldown = 3f;
     private float dashTimer = 0f;
 
+    [Header("Lanterns")]
+    private List<LanternBehaviour> lanterns;
+    public float lanternSpacing = 1.5f;
+
     //Runs before all Start functions
     private void Awake() {
-
+        lanterns = new List<LanternBehaviour>();
         //Ensures that the inputs have been initialized
         if (controls == null)
             controls = new InputMaster();
@@ -34,13 +38,38 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update() {
-        //Gets the current movement input value
+        //Gets the current movement input value, used in FixedUpdate to move the player.
         direction = controls.Player.Movement.ReadValue<Vector2>();
+
+        if (lanterns.Count > 0) {
+            /*
+            Vector3 averagePoint = Vector3.zero;
+            float tempX = 0;
+            float tempY = 0;
+            for (int i = 0; i < lanterns.Count; i++) {
+                tempX += lanterns[i].transform.position.x;
+                tempY += lanterns[i].transform.position.y;
+            }
+
+            Vector2 lanternsAveragePosition = new Vector2(tempX / lanterns.Count, tempY / lanterns.Count);
+            Vector2 groupAveragePosition = (lanternsAveragePosition + (Vector2)transform.position) / 2;
+            print(groupAveragePosition);
+            foreach (LanternBehaviour lantern in lanterns) {
+                lantern.Move(groupAveragePosition - (Vector2)lantern.transform.position);
+            }
+            */
+
+            foreach (LanternBehaviour lantern in lanterns)
+            {
+                lantern.Move(CalculateMove(lantern, GetNearbyObjects(lantern)));
+            }
+        }
+
 
         //Dash cooldown timer
         if (dashTimer > dashCooldown) {
@@ -48,6 +77,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //Movement is handled here for a smoother experience.
     private void FixedUpdate() {
         transform.Translate(direction * movementSpeed * Time.fixedDeltaTime);
     }
@@ -57,11 +87,57 @@ public class PlayerController : MonoBehaviour
 
         //If the object is a lantern
         if (lantern != null) {
-            //If the lantern isn't active
+            //If the lantern isn't active, activate it and add it to the list.
             if (!lantern.GetActive()) {
-                lantern.Activate(transform);
                 print("Activated lantern");
+
+                lantern.Activate(transform);
+                lanterns.Add(lantern);
             }
         }
+    }
+
+    private List<Transform> GetNearbyObjects (LanternBehaviour lantern) {
+        List<Transform> context = new List<Transform>();
+        Collider2D[] contextColliders = Physics2D.OverlapCircleAll(lantern.transform.position, lanternSpacing);
+
+        //Loops through each collider, checks if they're a lantern. If they're not the current lantern, add them to the list.
+        foreach (Collider2D col in contextColliders) {
+            LanternBehaviour lanternBehaviour = col.GetComponent<LanternBehaviour>();
+            if (lanternBehaviour != lantern && lanternBehaviour != null) {
+                context.Add(col.transform);
+            }
+        }
+        return context;
+    }
+
+    //Calculates cohesion and avoidance for individual lanterns.
+    private Vector2 CalculateMove(LanternBehaviour lantern, List<Transform> lanternsToAvoid) {
+        if (lanterns.Count == 0)
+            return Vector2.zero;
+
+        //Finds the average position of the group
+        Vector2 cohesionMove = Vector2.zero;
+        foreach (LanternBehaviour item in lanterns) {
+            cohesionMove += (Vector2)item.transform.position;
+        }
+        cohesionMove /= lanterns.Count;
+
+        cohesionMove = ((Vector2)transform.position + cohesionMove) / 2;
+
+        //Centers the vector on the lantern
+        cohesionMove -= (Vector2)lantern.transform.position;
+
+        Vector2 avoidanceMove = Vector2.zero;
+        foreach (Transform item in lanternsToAvoid) {
+            avoidanceMove += (Vector2)(lantern.transform.position - item.position);
+        }
+
+        if (lanternsToAvoid.Count > 0) {
+            avoidanceMove /= lanternsToAvoid.Count;
+        }
+
+
+        return cohesionMove + avoidanceMove;
     }
 }

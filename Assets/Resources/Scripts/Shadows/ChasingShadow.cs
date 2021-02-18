5648f56f607475b78569ddbff3f9cfefcb51ff91
Leon Chen <leonchen@live.nl>
2021-02-18 20:32:22 +0100

    changed the chasing shadow a little bit
    instead of dying instantly it will fade out slowly and then it'll go back to it's original spot. If the player goes back in the danger zone while he's fading out then it'll fade back in and keep chasing the player (so don't do that ^^)

--- Assets/Resources/Scripts/Shadows/ChasingShadow.cs
+++ Assets/Resources/Scripts/Shadows/ChasingShadow.cs
@@ -1,19 +1,25 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ChasingShadow : MonoBehaviour
 {
     public float speed = 5f;
+    public float fadeTimer = 1f;
     public GameObject spriteObject;
 
     private Vector3 originalPosition;
     private GameObject playerGameObject;
+    private SpriteRenderer spriteRenderer;
     private bool chasing;
+
+    private Coroutine currentCoroutine;
 
     void Start()
     {
         originalPosition = spriteObject.transform.position;
+
+        spriteRenderer = spriteObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0f);
     }
 
     void FixedUpdate()
@@ -34,6 +40,13 @@ public class ChasingShadow : MonoBehaviour
             }
 
             chasing = true;
+
+            if(currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+            }
+
+            currentCoroutine = StartCoroutine(FadeInSprite());
         }
     }
 
@@ -41,9 +54,37 @@ public class ChasingShadow : MonoBehaviour
     {
         if (collision.gameObject.tag == Constants.Tags.Player)
         {
-            MoveBackToOriginalPosition();
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+            }
+
+            currentCoroutine = StartCoroutine(FadeOutSprite());
         }
     }
+
+    private IEnumerator FadeInSprite()
+    {
+        for (float f = spriteRenderer.color.a; f <= 1f; f += Time.deltaTime / fadeTimer)
+        {
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, f);
+            yield return null;
+        }
+
+        currentCoroutine = null;
+    }
+    private IEnumerator FadeOutSprite()
+    {
+        for (float f = spriteRenderer.color.a; f >= 0f; f -= Time.deltaTime / fadeTimer)
+        {
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, f);
+            yield return null;
+        }
+
+        currentCoroutine = null;
+        MoveBackToOriginalPosition();
+    }
+
 
     private void MoveTowardsPlayer()
     {
